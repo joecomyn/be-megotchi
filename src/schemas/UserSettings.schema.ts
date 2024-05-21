eeef80e7401db60a193501ca4cd2adaa31fe7982
Joseph Comyn <joecomyn@hotmail.com>
2024-05-21 11:47:21 +0100

    Will track optional user settings linked to user objects through mongodb

---
+++ src/schemas/UserSettings.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({versionKey: false})
+export class UserSettings {
+    
+    @Prop({required: false})
+    receiveNotifications?: boolean;
+
+    @Prop({required: false})
+    receiveEmails?: boolean;
+
+    @Prop({required: false})
+    receiveSMS?: boolean;
+
+}
+
+export const UserSettingsSchema = SchemaFactory.createForClass(UserSettings)
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,22 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "src/schemas/User.schema";
 import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller"
+import { UserSettings, UserSettingsSchema } from "src/schemas/UserSettings.schema";
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{
-            name: User.name,
-            schema: UserSchema
-        }])
+        MongooseModule.forFeature(
+        [
+            {
+                name: User.name,
+                schema: UserSchema
+            },
+            {
+                name: UserSettings.name,
+                schema: UserSettingsSchema
+            }
+
+        ])
 ],
 providers:[UsersService],
 controllers:[UsersController],
